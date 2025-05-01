410fd4786d672d9c02f8a1e8cce5f1d751ed14ee
Med Nour <sosi66581@gmail.com>
2025-05-01 01:17:59 +0100

    _

--- src/Redux/Store/store.js
+++ src/Redux/Store/store.js
@@ -1,8 +1,7 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../Slices/authSlice';
-import departementReducer from '../Slices/departementSlice';
-import userReducer from '../Slices/userSlice';
+
 export const store = configureStore({
   reducer: {
     auth: authReducer,
